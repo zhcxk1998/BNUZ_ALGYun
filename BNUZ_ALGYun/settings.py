eff298d18a25b0882ee4a65c5769ddff3e46e800
DESKTOP-ASTA59B\DICKQI <dickqi87@gmail.com>
2018-04-11 02:00:30 +0800

    no message

--- BNUZ_ALGYun/settings.py
+++ BNUZ_ALGYun/settings.py
@@ -124,11 +124,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+STATIC_ROOT = os.path.join(BASE_DIR, "common_static/")
 # 配置静态文件目录
-STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, 'common_static'),
-    ]
+# STATICFILES_DIRS = [
+#         os.path.join(BASE_DIR, 'common_static'),
+#     ]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
